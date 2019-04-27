81ef8d83b58f80f74a7b1a6285bf15983c064c1f
Josh-Bennett <joshua.bennett@students.mq.edu.au>
2019-04-28 03:21:34 +1000

    Made CreateMakePredictions pretty

--- lockd/src/pages/make_prediction/CreateMakePrediction.js
+++ lockd/src/pages/make_prediction/CreateMakePrediction.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Segment, Container, Button, Header, Grid, Divider, TextArea } from 'semantic-ui-react';
+import { Segment, Container, Button, Header, Grid, Divider, TextArea, Input } from 'semantic-ui-react';
 import ExtractFile from '../../ExtractFile';
 
 export default class CreateMakePrediction extends Component {
@@ -9,9 +9,35 @@ export default class CreateMakePrediction extends Component {
 
     render() {
         return (
-            <Container style={{backgroundColor: "gray", minHeight: '100vh'}}>
-            ok making prediction
+          <Container>
+            <Container style={{backgroundColor: "#9AF36D", minHeight: '50vh', marginTop: "40px"}}>
+            <Container>
+            <p style={{fontSize : "30px"}}>Create A New Prediction</p>
+            <Input
+            style={{marginTop : "40px"}}
+            icon='tags'
+            iconPosition='left'
+            labelPosition='right'
+            placeholder='Enter tags'
+
+            />
+            </Container>
+            <Container>
+            <Input style ={{fontSize: "26px", align: "center"}} focus placeholder='Prediction...' />
+            </Container>
+            </Container>
+
+            <Container style = {{backgroundColor: "#D1FA89", minHeight : "50vh"}}>
+            <p style={{fontSize : "30px"}}>Similar Posts</p>
+            <p>There are no similar posts
+            </p>
+
+            </Container>
             </Container>
         )
     }
+
+
 }
+
+//export default InputExampleFocus
