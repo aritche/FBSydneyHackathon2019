50cde31eede2cff4e9934af971dec463c97e5d5c
Ari <ari.tchetchenian@gmail.com>
2019-04-27 17:48:17 +1000

    search bar in navbar

--- lockd/src/navigation/NavBar.js
+++ lockd/src/navigation/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Menu, Container} from 'semantic-ui-react';
+import {Menu, Container, Input} from 'semantic-ui-react';
 
 export default class NavBar extends Component {
     constructor(){
@@ -10,20 +10,23 @@ export default class NavBar extends Component {
         }
     };
 
-    onQueryChange = (e) => {
+    updateQuery = (e) => {
         this.setState({ query: e.target.value });
-    };
+    }
 
-    onQuerySubmit = () => {
-        alert("Searched for:  " + this.state.query);
-    };
+    performSearch = (e) => {
+        if (e.key === 'Enter'){
+            alert("You entered: " + this.state.query);
+        }
+    }
+
 
     render() {
 
         return (
-            <Menu inverted pointing secondary style={{backgroundColor: '#193446', boxShadow: '2px 1px 2px #000000', maxHeight: '45px'}}>
+            <Menu inverted pointing secondary style={{backgroundColor: 'black', padding: '10px', boxShadow: '2px 1px 2px #000000', maxHeight: '45px'}}>
               <Container style={{color:'white'}}>
-                    NAVBAR CONTENT
+                <Input icon='search' placeholder='Search for a prediction' onChange={this.updateQuery} onKeyPress={this.performSearch}/>
               </Container>
             </Menu>
 
