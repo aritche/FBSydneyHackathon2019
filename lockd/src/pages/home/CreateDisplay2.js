2a68c4ad0726298c888dd5319e46b903ef1ec0af
Ari <ari.tchetchenian@gmail.com>
2019-04-28 06:40:41 +1000

    better

---
+++ lockd/src/pages/home/CreateDisplay2.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Container, Grid } from "semantic-ui-react";
+import ExtractFile from "../../ExtractFile";
+
+export default class createDisplay extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: []
+    };
+
+    var extractor = new ExtractFile("fake_database/list.txt");
+
+    function post(prediction, tag1, tag2, likes, contributions) {
+      this.prediction = prediction;
+      this.tag1 = tag1;
+      this.tag2 = tag2;
+      this.likes = likes;
+      this.contributions = contributions;
+    }
+
+    console.log(extractor.getPosts().length);
+    for (var i = 0; i < extractor.getPosts().length; i++) {
+      console.log("abc" +  i);
+      console.log(extractor.getPosts().length)
+      console.log(extractor.getPredictions(i+1)[0].prediction)
+      console.log(extractor.getPostByID(i + 1).tags[0])
+      console.log(extractor.getPostByID(i + 1).tags[1])
+      console.log(extractor.getPredictions(i+1)[0].likes)
+      console.log(extractor.getPostByID(i+1).contributions[0])
+      this.state.posts.push(
+        new post(
+          extractor.getPredictions(i + 1)[0].prediction,
+          extractor.getPostByID(i + 1).tags[0],
+          extractor.getPostByID(i + 1).tags[1],
+          extractor.getPredictions(i+1)[0].likes,
+          extractor.getPostByID(i+1).contributions[0]
+        )
+      )
+    }
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.posts.map(post => 
+          <Grid style={{ border: "2px solid black" }}>
+            <Grid.Row>
+              <Grid columns={2} padded>
+                <Grid.Column style={{ border: "1px solid black" }}>
+                  <div>{post.tag1}</div>
+                </Grid.Column>
+                <Grid.Column style={{ border: "1px solid black" }}>
+                  <div>{post.tag2}</div>
+                </Grid.Column>
+              </Grid>
+            </Grid.Row>
+
+            <Grid.Row
+              style={{
+                paddingTop: "20px",
+                paddingBottom: "20px",
+                border: "1px solid black",
+                fontSize: "20px",
+                justifyContent: "center"
+              }}
+            >
+              <div>{post.prediction}</div>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Grid columns={3} padded>
+                <Grid.Column>
+                  <div>{post.likes}</div>{" "}
+                </Grid.Column>
+                <Grid.Column style={{ justifyContent: "center" }}>
+                  <div>{post.contributions}</div>
+                </Grid.Column>
+                <Grid.Column style={{ justifyContent: "right" }}>
+                  arijoshjoshjosfer
+                </Grid.Column>
+              </Grid>
+            </Grid.Row>
+          </Grid>
+        )}
+      </Container>
+    );
+  }
+}
--- lockd/src/pages/make_prediction/CreateMakePrediction.js
+++ lockd/src/pages/make_prediction/CreateMakePrediction.js
@@ -18,7 +18,6 @@ export default class CreateMakePrediction extends Component {
     componentDidMount() {
         setInterval(() => {
             this.setState(() => {
-                console.log('setting state');
                 return { unseen: "does not display" }
             });
         }, 1000);
@@ -34,23 +33,17 @@ export default class CreateMakePrediction extends Component {
             var sim = new Similarity();
 
             var array = sim.getSimilar(this.state.query);
-            console.log(array);
             // CODE FOR WHEN ENTER IS PRESSED
             // STRING YOU ENTERED IS STORED IN: this.state.query
             for (var item = 0; item<array.length; item++){
-                this.state.similar.push(array[item].prediction);
+                this.state.similar.push([array[item].prediction, array[item].postID]);
                 //this.setState((state) => {similar: this.state.similar.push(array[item].prediction)});
-                console.log(this.state.similar);
             }
         }
-
-
-
 
     }
 
     render() {
-        console.log('render called');
         return (
             <Container>
                 <Container style={{backgroundColor: "#9AF36D", minHeight: '50vh', marginTop: "40px"}}>
@@ -73,8 +66,8 @@ export default class CreateMakePrediction extends Component {
                 <Container style = {{backgroundColor: "#D1FA89", minHeight : "50vh"}}>
                     <p style={{fontSize : "30px"}}>Similar Posts</p>
                     {this.state.similar.map(sim=>
-                    <Container as = {Link} to = {{'/prediction/'+{(sim[0].postID).toString(10)}}} style={{ border: "0.5px solid grey", background: "white", width: "{10}" }}>
-                        {sim}
+                    <Container as = {Link} to={'/prediction/'+sim[1]} style={{ border: "0.5px solid grey", background: "white", width: "{10}" }}>
+                        {sim[0]}
                     </Container>
                         )}
                         </Container>
